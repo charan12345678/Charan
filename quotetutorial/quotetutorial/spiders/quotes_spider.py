b84a79f32974abca646461714d634f7a402e48d6
charanr52 <charanr52@gmail.com>
2020-06-23 01:15:15 +0530

    new changes

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -8,23 +8,25 @@ class QuoteSpider(scrapy.Spider):
     page_number = 2
     start_urls = [
         'https://www.amazon.in/s?k=phones&ref=nb_sb_noss_2'
+        
     ]
-
     def parse(self,response):
         items = QuotetutorialItem()
+        all_items = response.css('div.s-latency-cf-section')
+        for i in all_items:
+         product_name = i.css('.a-color-base.a-text-normal::text').extract()
+         price = i.css('.a-price-whole').css('::text').extract()
+         rating = i.css('.aok-align-bottom').css('::text').extract()
 
-        product_name = response.css('.a-color-base.a-text-normal::text').extract()
-        price = response.css('.a-price-whole').css('::text').extract()
-        rating = response.css('.aok-align-bottom').css('::text').extract()
-
-        items['product_name'] = product_name
-        items['price'] = price
-        items['rating'] = rating
+         items['product_name'] = product_name
+         items['price'] = price
+         items['rating'] = rating
             
-        yield items
+         yield items
+         
 
-        next_page = 'https://www.amazon.in/s?k=phones&page='+ str(QuoteSpider.page_number) +'&qid=1592587195&ref=sr_pg_2'
-        if QuoteSpider.page_number<=100:
-            QuoteSpider.page_number +=1
-            yield response.follow(next_page, callback = self.parse)
+        #next_page = 'https://www.amazon.in/s?k=phones&page='+ str(QuoteSpider.page_number) +'&qid=1592587195&ref=sr_pg_2'
+        #if QuoteSpider.page_number<=100:
+        #   QuoteSpider.page_number +=1
+        #  yield response.follow(next_page, callback = self.parse)
             
