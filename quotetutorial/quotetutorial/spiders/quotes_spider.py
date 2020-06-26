7da809a94807d4a1f13ed1c05e604d61d3b104da
charanr52 <charanr52@gmail.com>
2020-06-27 00:59:29 +0530

    latest

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -8,26 +8,33 @@ class QuoteSpider(scrapy.Spider):
     name = 'amazon'
     page_number = 2
     start_urls = [
-        'https://www.amazon.in/s?k=phones&ref=nb_sb_noss_2'
-        
+        'https://www.amazon.in/s?k=phones&ref=nb_sb_noss_2'   
     ]
 
     def parse(self,response):
-        #items = QuotetutorialItem()
+        items = QuotetutorialItem()
         all_items = response.css('div.s-latency-cf-section')
         for i in all_items:
-         #product_name = i.css('.a-color-base.a-text-normal::text').extract()
-         #price = i.css('.a-price-whole').css('::text').extract()
+         product_name = i.css('.a-color-base.a-text-normal::text').extract()
+         price = i.css('.a-price-whole').css('::text').extract()
          #rating = i.css('.aok-align-bottom').css('::text').extract()
          hrefs = i.css('.a-text-normal::attr(href)').extract_first()
 
-         #items['product_name'] = product_name
-         #items['price'] = price
+         items['product_name'] = product_name
+         items['price'] = price
          #items['rating'] = rating
          #items['hrefs'] = hrefs 
 
+         yield items
+
          if hrefs is not None:
              yield response.follow(hrefs,callback =self.parse_page)  
+
+        next_page = 'https://www.amazon.in/s?k=phones&page='+ str(QuoteSpider.page_number) +'&qid=1592587195&ref=sr_pg_2'
+        if QuoteSpider.page_number<=20:
+            QuoteSpider.page_number +=1
+            yield response.follow(next_page, callback = self.parse)
+
                 
          #yield items
             
@@ -41,8 +48,8 @@ class QuoteSpider(scrapy.Spider):
         #news = Newclass()
         #labels = response.css('.col1 .label::text').extract()
         values = response.css('.col1 .value::text').extract()
-        product_name = response.css('#productTitle::text').extract_first().strip()
-        price = response.css('#priceblock_ourprice::text').extract_first().strip() 
+        #product_name = response.css('#productTitle::text').extract_first().strip()
+        #price = response.css('#priceblock_ourprice::text').extract_first().strip() 
 
 
             
@@ -51,8 +58,8 @@ class QuoteSpider(scrapy.Spider):
 
         #items['labels'] = labels
         items['values'] = values
-        items['product_name'] = product_name
-        items['price'] = price
+        #items['product_name'] = product_name
+        #items['price'] = price
         #items['rating'] = rating
 
         items['OS'] = items['values'][0]
@@ -64,9 +71,9 @@ class QuoteSpider(scrapy.Spider):
         items['connectivity'] = items['values'][6]
         items['special_features'] = items['values'][7]
         items['Display_technology'] = items['values'][8]
-        items['other_camera_features'] = items['values'][9]
-        items['form_factor'] = items['values'][10]
-        items['Battery_Power_Rating'] = items['values'][11]
+        items['camera'] = items['values'][10]
+        items['form_factor'] = items['values'][9]
+        items['Battery_Power_Rating'] = items['values'][14]
 
         yield items
 
@@ -81,5 +88,3 @@ class QuoteSpider(scrapy.Spider):
         #if QuoteSpider.page_number<=50:
          #  QuoteSpider.page_number +=1
           # yield response.follow(next_page, callback = self.parse)
-        
-            
